1e55b8a237b50fafc0d61b9a13ce507cca5be9c3
Technochips <julienfillol2@gmail.com>
2017-06-05 18:18:18 +0200

    bot can log-in

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("This isn't ready yet.");
-return 1;
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () =>
+{
+	console.log('Ready');
+});
+
+console.log('Reading token file...');
+fs.readFile('./.token', 'utf8', function(err, data)
+{
+	if(err)
+	{
+		console.log('Something bad happened :(');
+		if(err.code = 'ENOENT')
+		{
+			console.log('Couldn\'t read the .token file. Make sure it exists, it should contains your bot\'s token.');
+		}
+		return 1;
+	}
+	console.log('Trying to log-in...');
+	client.login(data);
+});
